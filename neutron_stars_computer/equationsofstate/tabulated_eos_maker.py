ccaf840d504ed39e1be7b49cff235b814110e22d
Gustavo Schwantz <gustavo_aso1994@hotmail.com>
2026-05-16 19:12:55 -0300

    Adding tabulated_eos_maker.py.

--- neutron_stars_computer/equationsofstate/tabulated_eos_maker.py
+++ neutron_stars_computer/equationsofstate/tabulated_eos_maker.py
@@ -5,14 +5,12 @@ from neutron_stars_computer.equationsofstate.fermionic_dm import FermionicDarkMa
 import numpy as np
 import pandas as pd
 
-def main() -> None:
-    MF = 2
 
+def table_maker(MF: float) -> None:
     eos: EquationOfState = FermionicDarkMatter(MF)
 
     data_list = []
 
-    #for z in np.geomspace(1e-3, 1e0, 200):
     for z in np.geomspace(1e-1, 1e1, 200):
 	    e = eos.energy_density(z)*cf.GEV4_TO_MEVFM3
 	    p = eos.pressure(z)*cf.GEV4_TO_MEVFM3
@@ -22,8 +20,5 @@ def main() -> None:
 
     tabulated_eos = pd.DataFrame(data_list)
 
-    tabulated_eos.to_csv(f'./neutron_stars_computer/equationsofstate/tabulated_eos/DM_EOS/f_dm_m{MF}_y0.csv',
+    tabulated_eos.to_csv(f'/home/gustavo/Quarks/neutron_stars_computer/equationsofstate/tabulated_eos/DM_EOS/f_dm_m{MF}_y0.csv',
 	                        index = False, sep = ' ', float_format = '%.5e')
-
-if __name__ == '__main__':
-    main()	
