4ebb6f36315371daf8c9073e3b583b01c31373bb
Gustavo Schwantz <gustavo_aso1994@hotmail.com>
2026-05-16 20:16:43 -0300

    Adding eos_plotter.py.

--- main_op.py
+++ main_op.py
@@ -4,6 +4,9 @@ from neutron_stars_computer.star.factory import StarStabilityFactory, StarFactor
 from neutron_stars_computer.star.constellation import create_stellar_family
 from neutron_stars_computer.equationsofstate.eos import EquationOfState
 from neutron_stars_computer.equationsofstate.massless_mit_bm import MasslessMITBM
+from neutron_stars_computer.equationsofstate.interpolate import RIPEOS
+from neutron_stars_computer.equationsofstate.cfl import CFL
+from neutron_stars_computer.figures_two_fluid.eos_plotter import plot_eos
 
 import time
 import numpy as np
@@ -11,11 +14,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def main() -> None:
-    #print('Starting...')
-    #start = time.time()
+    print('Starting...')
+    start = time.time()
 
-    eos: EquationOfState = MasslessMITBM(60)
-    central_pressures = np.geomspace(1e-1, 1e4, 100)
+    #eos: EquationOfState = MasslessMITBM(60)
+    #central_pressures = np.geomspace(1e-1, 1e4, 100)
     #tov_input = TOVInput(
     #    eos,
     #    ABSOLUTE_TOLERANCE=[1e-4, 1e-4, 1e-6],
@@ -30,17 +33,19 @@ def main() -> None:
     #)
     #print(stars)
 
-    #end = time.time()
-    #print(f"Time to complete: {end - start:.2f} s")
+    plot_eos([MasslessMITBM(60), MasslessMITBM(90), 
+                RIPEOS("./neutron_stars_computer/equationsofstate/tabulated_eos/DM_EOS/f_dm_m1_y0.csv"),
+                RIPEOS("./neutron_stars_computer/equationsofstate/tabulated_eos/DM_EOS/f_dm_m0.5_y0.csv"),
+                RIPEOS("./neutron_stars_computer/equationsofstate/tabulated_eos/DM_EOS/f_dm_m2_y0.csv"),
+                CFL(70)], 
+                ['MIT60', 'MIT90', 'FF1', 'FF0.5', 'FF2', 'CFL'],
+                ['--', ':', '--', '-', ':', '-'],
+                ['red', 'green', 'cyan', 'blue', 'magenta', 'black'],
+                np.geomspace(1e-1, 1e4, 100), (1e0, 1e5), (1e-1, 1e4), 4, "/home/gustavo/Quarks//Huang_2026/Figure_1_2.png")
 
-    central_densities = np.array([eos.energy_density_from(p) for p in central_pressures])
+    end = time.time()
+    print(f"Time to complete: {end - start:.2f} s")
 
-    df = pd.DataFrame({'pressures': central_pressures, 'densities': central_densities})
-
-    print(df)
-
-    df.plot('densities', 'pressures', loglog=True, xlim=(1e0, 1e5), ylim=(1e0, 1e4))
-    plt.savefig('fig.png')
 
 if __name__ == '__main__':
     main()
---
+++ neutron_stars_computer/figures_two_fluid/eos_plotter.py
@@ -0,0 +1,23 @@
+from neutron_stars_computer.equationsofstate.eos import EquationOfState
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+Array = np.ndarray
+
+def plot_eos(EOSs: list[EquationOfState], labels: list[str], linestyles: list[str], colors: list[str],
+	pressures: Array, xlim: tuple,  ylim: tuple, linewidth: float, path: str) -> None:
+
+	fig, ax = plt.subplots()
+
+	for eos, label, linestyle, color in zip(EOSs, labels, linestyles, colors):
+		densities = np.array([eos.energy_density_from(p) for p in pressures])
+
+		pd.DataFrame({'p': pressures, 'e': densities}).plot('e', 'p', loglog=True, xlim=xlim, ylim=ylim, ax=ax, 
+			label=label, linestyle=linestyle, color=color, linewidth=linewidth)
+
+
+	plt.xlabel(r'$\epsilon$ [MeV/fm$^3$]')
+	plt.ylabel(r'p [MeV/fm$^3$]')
+	plt.savefig(path)   
